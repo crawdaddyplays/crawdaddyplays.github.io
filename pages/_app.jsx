27e2b13652096b42f7d4eb3a62b95abbc08f433c
Josh Quintana <josh@highwaybenefits.com>
2026-02-04 15:32:41 -0500

    simple nav

---
+++ pages/_app.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+export default function MyApp({ Component, pageProps }) {
+  const links = [
+    '/',
+    '/home',
+    '/home/alerts',
+  ]
+  return <>
+    <ul>
+    {links.map(l => <li key={l}><Link href={l}>Go To {l}</Link></li>)}
+    </ul>
+ 
+    <Component {...pageProps} />
+    </>
+}
+
