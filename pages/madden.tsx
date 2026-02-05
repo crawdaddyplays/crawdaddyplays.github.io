03474902f530f4e15cc876a72bd95d7141ab8aeb
Josh Quintana <josh@highwaybenefits.com>
2026-02-04 19:33:41 -0500

    auto calculate and sort by points

--- pages/madden.tsx
+++ pages/madden.tsx
@@ -1,10 +1,11 @@
 import App, { AppContext, AppInitialProps, AppProps } from 'next/app'
 
 import './madden.css'; // ← put the CSS above in this file
-import rankings from './madden_rankings.json'
+import prernk from './madden_rankings.json'
 type AppOwnProps = { example: string }
 
 const MaddenRankings = () => {
+  const rankings = prernk.sort((a, b) => b.points - a.points)
   return (
     <div className="container">
     <h1>MADDEN 1V1</h1>
@@ -19,9 +20,9 @@ const MaddenRankings = () => {
     </tr>
     </thead>
     <tbody>
-    {rankings.map((player) => (
-      <tr key={player.rank} className="row">
-      <td className="td rank">{player.rank}.</td>
+    {rankings.map((player, i) => (
+      <tr key={i} className="row">
+      <td className="td rank">{i + 1}.</td>
       <td className="td player">
       <img src={player.avatar} alt={player.name} className="avatar" />
       {player.special === 'banned' ? (
